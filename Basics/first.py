1701ae20f3df69f1fb05b4de62af5eacc673ab7c
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2026-07-09 03:28:16 -0700

    Variables in Python

--- Basics/first.py
+++ Basics/first.py
@@ -1,1 +1,24 @@
-print("hello, world")
+print("hello, world,", end=" ")
+print("Welcome to Python Programming")
+print("Good Morning, \n Thank you");
+
+print(1, end = "\n");
+print(2);
+
+a = "hello";
+b = "world";
+print(a + b); # concatenation of two strings
+
+name = "Gaurav";
+
+age = 22;
+
+print("My name is " + name + " and I am " + str(age) + " years old.");
+
+
+# string formatting using f-string
+
+print(f"My name is {name} and I am {age} years old.");
+
+print("Gaurav \t Vishwakarma") # is used to add tab space in the output
+
---
+++ Variables/programme.py
@@ -0,0 +1,44 @@
+# Valid
+name = "Alex"
+_age = 25
+user_1 = "admin"
+totalMarks = 90
+
+# Invalid
+""" 1name = "Alex"      # starts with digit
+user-name = "Alex"  # hyphen not allowed
+class = "test"      # reserved keyword
+user name = "Alex"  # space not allowed """
+
+
+name = "Gaurav";
+print(name);
+
+_name = "Vishwakarma";
+print(_name);
+
+AGE = 22;
+print(AGE); 
+
+AGE += 1; # incrementing the value of AGE by 1
+print(AGE);
+
+print("My name is " + name + " and I am " + str(AGE) + " years old.");
+
+print(f"My name is {name} and I am {AGE} years old.");
+
+name = str("Gaurav");
+age = str(22);
+percentage = str(90.5);
+
+print("My name is " + name + " and I am " + age + " years old. My percentage is " + percentage + "%.");
+
+
+
+name = str("rahul");
+age = int(32);
+percentage = float(90.5);
+
+print(name , type(name));
+print(age , type(age));
+print(percentage , type(percentage));
