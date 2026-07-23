8e5144794c74c7e803601904a4ca224ddd443e88
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2026-07-23 03:51:51 -0700

    Inputs

---
+++ Input/programme.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+print(f"Hey, {name}! What's up?")
+
+float_num = float(input("Enter a float number: "))
+print(f"The float number you entered is: {float_num}")
+
+int_num = int(input("Enter an integer number: "))
+print(f"The integer number you entered is: {int_num}")
+
+bool_value = input("Enter a boolean value (True/False): ")
+print(f"The boolean value you entered is: {bool_value}")
+
+file_path = input("Enter the file path: ")
+print(f"The file path you entered is: {file_path}")
+
+float_round = round(float_num, 2)
+print(f"The float number rounded to 2 decimal places is: {float_round}")    
